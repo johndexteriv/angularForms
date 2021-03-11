a2c8f6e7bd1108a60cfe09d2fc6f3fa1be5c5123
John Garner <jdextergarneriv@gmail.com>
2021-03-11 14:17:07 -0500

    use form data to display user{} information if submitted - .reset form

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -9,22 +9,50 @@ import { NgForm } from "@angular/forms";
 export class AppComponent {
   @ViewChild("f") signupForm: NgForm;
   defaultQuestion = "pet";
+  defaultGender = "male";
   answer = "";
   genders = ["male", "female"];
+  user = {
+    username: "",
+    email: "",
+    secretQuestion: "",
+    answer: "",
+    gender: "",
+  };
+  submitted = false;
+
   suggestUserName() {
     const suggestedName = "Superuser";
+    // this.signupForm.setValue({
+    // });
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName,
+      },
+    });
   }
 
   // onSubmit(form: NgForm) {
   //   console.log(form);
   // }
 
+  // onSubmit() {
+  //   console.log(this.signupForm);
+  //   if (this.signupForm.controls.email.valid) {
+  //     console.log("This form is valid thank you for entering a username");
+  //   } else {
+  //     console.log("This form is not valid please enter a valid email address");
+  //   }
+  // }
+
   onSubmit() {
-    console.log(this.signupForm);
-    if (this.signupForm.controls.email.valid) {
-      console.log("This form is valid thank you for entering a username");
-    } else {
-      console.log("This form is not valid please enter a valid email address");
-    }
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionsAnswer;
+    this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
   }
 }
